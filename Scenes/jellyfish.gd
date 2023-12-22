5a242b85e49a6e8554497c1986e0c99b0641accc
cyrus bhimji <cyrusbhimji@students.berkeley.net>
2023-12-22 09:00:27 -0800

    d

--- Scenes/jellyfish.gd
+++ Scenes/jellyfish.gd
@@ -1,28 +1,20 @@
-extends StaticBody2D
+extends Area2D
 
 var oxygen_regain = 10
 var gyat = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var targets = $Area2D2.has_overlapping_bodies()
-		
-	if targets == true and gyat == false:
-		give_oxygen()
-
+	if gyat == true:
+		pass
+	
 func give_oxygen():
 	$/root/MainGame/Player/water_time.time -= oxygen_regain
 	gyat = true
 	$/root/MainGame/pop_timer.start()
 	$/root/MainGame/jellyfish.process_mode = 4
 	$/root/MainGame/jellyfish.visible = false
-	
-
+	pass
 
 
 func _on_pop_timer_timeout():
@@ -30,3 +22,12 @@ func _on_pop_timer_timeout():
 	gyat = false
 	$/root/MainGame/jellyfish.process_mode = 0
 	$/root/MainGame/jellyfish.visible = true
+	
+
+
+
+func _on_body_entered(body):
+	if body.name == "Player" and gyat == false:
+		give_oxygen()
+		
+
