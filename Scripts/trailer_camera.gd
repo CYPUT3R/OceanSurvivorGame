d27b25aea464fe3c7e1df1c59abadd4604465c37
cyrus bhimji <cyrusbhimji@students.berkeley.net>
2024-04-26 09:03:31 -0700

    Signed-off-by: cyrus bhimji <cyrusbhimji@students.berkeley.net>

---
+++ Scripts/trailer_camera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("camera_left", "camera_right")
+	if direction:
+		position.x += direction * SPEED
+
+		
